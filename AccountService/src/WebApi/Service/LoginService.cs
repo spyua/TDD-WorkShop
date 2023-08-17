9a16017ced5242127d95b931504b30dfc04b6c9e
pal2097 <pal2097@gmail.com>
2023-08-17 14:33:20 +0800

    WIP

---
+++ AccountService/src/WebApi/Service/LoginService.cs
@@ -0,0 +1,38 @@
+﻿namespace WebApi
+{
+    public class LoginService
+    {
+        private int _failedLoginAttempts = 0;
+        private bool _isAccountLocked = false;
+
+        public bool IsAccountLocked(string v)
+        {
+            return _isAccountLocked; // 返回帳戶是否被鎖定
+        }
+
+        public bool TryLogin(string username, string password)
+        {
+            if (_isAccountLocked)
+            {
+                return false; // 帳戶被鎖定
+            }
+
+            if (username == "validUser" && password == "validPassword")
+            {
+                _failedLoginAttempts = 0; // 重置失敗登入計數
+                return true; // 登入成功
+            }
+            else
+            {
+                _failedLoginAttempts++;
+
+                if (_failedLoginAttempts >= 3)
+                {
+                    _isAccountLocked = true; // 鎖定帳戶
+                }
+
+                return false; // 登入失敗
+            }
+        }
+    }
+}
--- AccountService/test/WebApi.Test/安全登入StepDefinitions.cs
+++ AccountService/test/WebApi.Test/安全登入StepDefinitions.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using System;
 using TechTalk.SpecFlow;
 
@@ -6,52 +7,65 @@ namespace WebApi.Test
     [Binding]
     public class 安全登入StepDefinitions
     {
+        private LoginService _loginService; // 假設有一個登入服務
+        private string _username;
+        private string _password;
+        private bool _loginResult;
+
+        public 安全登入StepDefinitions()
+        {
+            _loginService = new LoginService(); // 初始化登入服務
+        }
+
         [Given(@"一個有效的帳號和密碼")]
         public void Given一個有效的帳號和密碼()
         {
-            throw new PendingStepException();
+            _username = "validUser";
+            _password = "validPassword";
         }
 
         [When(@"用戶嘗試登入")]
         public void When用戶嘗試登入()
         {
-            throw new PendingStepException();
+            _loginResult = _loginService.TryLogin(_username, _password);
         }
 
         [Then(@"登入應成功")]
         public void Then登入應成功()
         {
-            throw new PendingStepException();
+            _loginResult.Should().BeTrue();
         }
 
         [Given(@"一個無效的帳號或密碼")]
         public void Given一個無效的帳號或密碼()
         {
-            throw new PendingStepException();
+            _username = "invalidUser";
+            _password = "invalidPassword";
         }
 
         [Then(@"登入應失敗")]
         public void Then登入應失敗()
         {
-            throw new PendingStepException();
+            _loginResult.Should().BeFalse();
         }
 
         [Given(@"用戶已經嘗試登入兩次並失敗")]
         public void Given用戶已經嘗試登入兩次並失敗()
         {
-            throw new PendingStepException();
+            _loginService.TryLogin("user", "wrongPassword");
+            _loginService.TryLogin("user", "wrongPassword");
         }
 
         [When(@"用戶再次嘗試登入並失敗")]
         public void When用戶再次嘗試登入並失敗()
         {
-            throw new PendingStepException();
+            _loginResult = _loginService.TryLogin("user", "wrongPassword");
         }
 
         [Then(@"帳戶應被鎖定")]
         public void Then帳戶應被鎖定()
         {
-            throw new PendingStepException();
+            _loginService.IsAccountLocked("user").Should().BeTrue();
         }
     }
 }
